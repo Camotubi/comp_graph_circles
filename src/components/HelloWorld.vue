dc171374336637b92dbfa04495f14298d230282e
Camotubi <camotubi@gmail.com>
2018-05-25 02:15:47 -0500

    linting

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -17,36 +17,36 @@ export default {
       msg: 'Welcome to Your Vue.js App'
     }
   },
-  methods:{
-    ddaCircle(radio) {
+  methods: {
+    ddaCircle (radio) {
       const canvasHeight = 200
       const canvasWidth = 200
       let rx = radio
       let x = Math.round(radio)
       let y = 0
-    new P5(sketch => {
-      sketch.setup = () => {
-        sketch.createCanvas(canvasHeight, canvasWidth)
-        sketch.background(0)
-      }
-      sketch.draw = () => {
-        sketch.stroke(255);
-        sketch.translate(canvasWidth/2, canvasHeight/2);
-        while( y <= x){
-          sketch.point(x, y)
-          sketch.point(y, x)
-          sketch.point(-x, y)
-          sketch.point(-y, x)
-          sketch.point(x, -y)
-          sketch.point(y, -x)
-          sketch.point(-x, -y)
-          sketch.point(-y, -x)
-          rx -= y/rx
-          x = Math.round(rx)
-          y++
+      new P5(sketch => {
+        sketch.setup = () => {
+          sketch.createCanvas(canvasHeight, canvasWidth)
+          sketch.background(0)
         }
-      }
-    }, 'p5sketch')
+        sketch.draw = () => {
+          sketch.stroke(255)
+          sketch.translate(canvasWidth / 2, canvasHeight / 2)
+          while (y <= x) {
+            sketch.point(x, y)
+            sketch.point(y, x)
+            sketch.point(-x, y)
+            sketch.point(-y, x)
+            sketch.point(x, -y)
+            sketch.point(y, -x)
+            sketch.point(-x, -y)
+            sketch.point(-y, -x)
+            rx -= y / rx
+            x = Math.round(rx)
+            y++
+          }
+        }
+      }, 'p5sketch')
     }
   }
 }
